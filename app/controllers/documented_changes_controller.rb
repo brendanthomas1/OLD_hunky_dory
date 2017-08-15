777d32c1a73d3a3db1ec850a956e2e89fe68f679
Brendan Thomas <bt331609@gmail.com>
2017-08-15 14:53:04 -0400

    update doc changes

--- app/controllers/documented_changes_controller.rb
+++ app/controllers/documented_changes_controller.rb
@@ -1,5 +1,5 @@
 class DocumentedChangesController < ApplicationController
-  before_action :set_documented_change, only: %i[show destroy]
+  before_action :set_documented_change, only: %i[show update destroy]
 
   def index
     @documented_changes = DocumentedChange.all
@@ -19,6 +19,14 @@ class DocumentedChangesController < ApplicationController
     else
       # TODO: add flash message
       render :new
+    end
+  end
+
+  def update
+    if @documented_change.update documented_change_params
+      redirect_to documented_changes_path
+    else
+      render :show
     end
   end
 
--- spec/requests/documented_change_spec.rb
+++ spec/requests/documented_change_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Requests to documented_changes/:id', type: :request do
-  let(:change) { create(:documented_change) }
+  let(:doc_change) { create(:documented_change) }
+  let(:url) { documented_change_path(doc_change) }
 
   describe 'HTML GET show' do
     it 'responds with an HTML 200' do
-      get documented_change_path(change), as: :html
+      get url, as: :html
       expect(response.status).to eq 200
       expect(response.content_type).to eq 'text/html'
     end
   end
 
+  describe 'HTML PUT' do
+    let(:params) { { documented_change: { summary: 'Updated' } } }
+
+    it 'responds with an HTML 302' do
+      put url, params: params, as: :html
+
+      expect(doc_change.reload.summary)
+        .to eq params[:documented_change][:summary]
+      expect(response.content_type).to eq 'text/html'
+      expect(response.status).to eq 302
+    end
+
+    context 'with invalid params' do
+      let(:params) { { documented_change: { summary: nil } } }
+
+      it 'does not update the documented change' do
+        expect { put url, params: params, as: :html }
+          .not_to(change { doc_change })
+      end
+    end
+  end
+
   describe 'HTML DELETE' do
     it 'deletes the Documented Change and responds with an HTML 302' do
-      delete documented_change_path(change)
-      expect { change.reload }.to raise_error ActiveRecord::RecordNotFound
+      delete url
+      expect { doc_change.reload }.to raise_error ActiveRecord::RecordNotFound
       expect(response.status).to eq 302
       expect(response.content_type).to eq 'text/html'
     end
